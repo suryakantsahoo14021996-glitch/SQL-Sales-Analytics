386345aa5f47aaf2bf7178df8f9c62a4b95cef2c
Suryakant Sahoo <suryakantsahoo14021996@gmail.com>
2026-08-20 16:48:04 +0530

    Update 02_data_aggregation.sql
    Worked on Quantity , Sales , Price data aggregation to check if any anomallies is there.
    Perform analysis on What is total sales , quantity , top perfromers region and category wise.
    
    I have used COUNT , SUM ,MAX , LIMIT , AVG , / , NULLIF , WHERE , GROUP BY , OPERATORS etc

--- sql/02_data_aggregation.sql
+++ sql/02_data_aggregation.sql
@@ -25,6 +25,63 @@ group by 1;
 Select Distinct(Subregion) from saless
 where Region= "AMER";
 
+select round(sum(Sales),2),
+max(Sales) as max_sales,
+min(Sales) as min_sales,
+avg(Sales) as avg_sales
+from saless;
+
+select * from saless
+where Sales < 0;
+
+Select 
+Region,
+round(sum(Sales),2) as total_sales
+ from saless
+ group by 1
+ order by 1 desc;
+ 
+ select 
+ Industry,
+ sum(Sales) as total_sales
+ from saless
+ group by 1
+ order by 1
+ limit 1;
+ 
+ select 
+ Customer,
+ Sales as total_sales
+ from saless
+ order by Sales desc
+ limit 5;
+ 
+ /*Quantity Aggregation*/
+select 
+sum(Quantity) as totla_qty,
+avg(Quantity) as avg_quantity,
+max(Quantity) as max_qty
+from saless; 
+
+select 
+Product , 
+sum(Quantity) as total_qty
+from saless
+group by 1
+order by total_qty desc
+limit 5;
+
+select 
+Region , 
+sum(Quantity) as total_qty
+from saless
+group by 1
+order by total_qty desc
+limit 5;
+
+
+ 
+
 
 
 
